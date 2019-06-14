846a75f8bfcdf0f7624f57640ccb5b8650282fba
Soosai Alexander <soosai_antony@hotmail.com>
2019-06-14 22:39:39 +0530

    Changes to pdf service

--- src/app/document-data-entry/FieldMapData.ts
+++ src/app/document-data-entry/FieldMapData.ts
@@ -1,6 +1,11 @@
 import { FieldMap } from './FieldMap';
 
 export const NonMedicalFieldMapping: FieldMap[] = [
+    { name: "PurposeOfInsurance", page: 1, posA: 110, posB: 160, posC: 180, posD: 10 },
+    { name: "Name", page: 1, posA: 110, posB: 170, posC: 190, posD: 45 }
+]
+
+export const MedicalFieldMapping: FieldMap[] = [
     { name: "PurposeOfInsurance", page: 2, posA: 110, posB: 160, posC: 180, posD: 10 },
     { name: "Name", page: 1, posA: 110, posB: 170, posC: 190, posD: 45 }
 ]
--- src/app/shared/generate-pdf.service.ts
+++ src/app/shared/generate-pdf.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { NonMedicalFieldMapping } from '../document-data-entry/FieldMapData';
+import { NonMedicalFieldMapping, MedicalFieldMapping }
+  from '../document-data-entry/FieldMapData';
 
 @Injectable({
   providedIn: 'root'
@@ -42,29 +43,26 @@ export class GeneratePdfService {
 
           var canvas: any = document.getElementById('the-canvas');
           var context = canvas.getContext('2d');
-          context.globalAlpha = 0.3;
-          context.fillStyle = "yellow";
-          context.fillRect(map.posA*scale, map.posB*scale, map.posC*scale, map.posD*scale)
+          var imageData = context.getImageData(map.posA * scale, map.posB * scale, map.posC * scale, map.posD * scale);
 
-          var imageData = context.getImageData(map.posA*scale, map.posB*scale, map.posC*scale, map.posD*scale);
-
-          // create destiantion canvas
           var canvas1 = document.createElement("canvas");
-          canvas1.width = map.posC*scale;
-          canvas1.height = map.posD*scale;
+          canvas1.width = map.posC * scale;
+          canvas1.height = map.posD * scale;
           var ctx1 = canvas1.getContext("2d");
-          ctx1.rect(0, 0, map.posC*scale, map.posD*scale);
+          ctx1.rect(0, 0, map.posC * scale, map.posD * scale);
           ctx1.fillStyle = 'white';
           ctx1.fill();
           ctx1.putImageData(imageData, 0, 0);
 
-          // put data to the img element
-          var dstImg:any = document.getElementById('newImg');
+          var dstImg: any = document.getElementById('newImg');
           dstImg.src = canvas1.toDataURL("image/png");
+
+          context.globalAlpha = 0.3;
+          context.fillStyle = "yellow";
+          context.fillRect(map.posA * scale, map.posB * scale, map.posC * scale, map.posD * scale)
         });
       });
     }, function (reason) {
-      // PDF loading error
       console.error(reason);
     });
 
